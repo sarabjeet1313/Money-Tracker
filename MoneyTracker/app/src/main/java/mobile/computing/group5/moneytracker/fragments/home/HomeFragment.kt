84257d9b5148d0531600581cbbb2c486afc94846
Deep Muni <deep.muni94@gmail.com>
2020-03-17 11:56:25 -0300

    Created Home Fragment with Calendar

--- MoneyTracker/app/src/main/java/mobile/computing/group5/moneytracker/fragments/home/HomeFragment.kt
+++ MoneyTracker/app/src/main/java/mobile/computing/group5/moneytracker/fragments/home/HomeFragment.kt
@@ -1,14 +1,16 @@
 package mobile.computing.group5.moneytracker.fragments.home
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fragment_home.*
 import mobile.computing.group5.moneytracker.R
+import java.util.*
+
 
 class HomeFragment : Fragment() {
 
@@ -19,12 +21,52 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        homeViewModel.text.observe(this, Observer {
-            Log.i("message", it)
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val MONTHS = arrayOf(
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        )
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        button_calender.setOnClickListener{
+            val dpd = context?.let { it1 ->
+                DatePickerDialog(
+                    it1,
+                    DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                        val date = MONTHS[monthOfYear] + " " + dayOfMonth + ", " + year
+                        dateText.text = date
+                    },
+                    year,
+                    month,
+                    day
+                )
+            }
+            if (dpd != null) {
+                dpd.show()
+            }
+        }
+
+        button_save.setOnClickListener {
+            Log.i("Message", input_description.text.toString())
+        }
     }
 }
--- MoneyTracker/app/src/main/java/mobile/computing/group5/moneytracker/fragments/home/HomeViewModel.kt
+++ MoneyTracker/app/src/main/java/mobile/computing/group5/moneytracker/fragments/home/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package mobile.computing.group5.moneytracker.fragments.home
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,4 +11,9 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    fun save(){
+        Log.i("Message", "Button clicked")
+    }
+
 }
