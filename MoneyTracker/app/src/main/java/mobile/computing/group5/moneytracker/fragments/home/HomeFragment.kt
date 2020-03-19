6e8ec8eb8e56e128030408e2b83f7c99fff2ff37
Deep Muni <deep.muni94@gmail.com>
2020-03-19 20:29:32 -0300

    Modified the UI for Home and Transaction Edit Fragment

--- MoneyTracker/app/src/main/java/mobile/computing/group5/moneytracker/fragments/home/HomeFragment.kt
+++ MoneyTracker/app/src/main/java/mobile/computing/group5/moneytracker/fragments/home/HomeFragment.kt
@@ -1,23 +1,24 @@
 package mobile.computing.group5.moneytracker.fragments.home
 
 import android.app.DatePickerDialog
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.fragment_home.*
 import mobile.computing.group5.moneytracker.R
+import mobile.computing.group5.moneytracker.model.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    var type = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,11 +31,9 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val adapter = context?.let { ArrayAdapter.createFromResource(it, R.array.type_arrays_type_home, R.layout.dropdown_item) }
-        adapter?.setDropDownViewResource(R.layout.dropdown_item)
-        dropdown_type.adapter = adapter
+        onLoad()
 
-        val MONTHS = arrayOf(
+        val listOfMonths = arrayOf(
             "January",
             "February",
             "March",
@@ -58,8 +57,8 @@ class HomeFragment : Fragment() {
             val dpd = context?.let { it1 ->
                 DatePickerDialog(
                     it1,
-                    DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                        val date = MONTHS[monthOfYear] + " " + dayOfMonth + ", " + year
+                    DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                        val date = listOfMonths[monthOfYear] + " " + dayOfMonth + ", " + year
                         dateText.text = date
                     },
                     year,
@@ -67,8 +66,21 @@ class HomeFragment : Fragment() {
                     day
                 )
             }
-            if (dpd != null) {
-                dpd.show()
+            dpd?.show()
+        }
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+
+            type = when {
+                    R.id.radioIncome == checkedId -> {
+                        "Income"
+                    }
+                    R.id.radioExpense == checkedId -> {
+                        "Expense"
+                    }
+                    else -> {
+                        ""
+                    }
             }
         }
 
@@ -77,22 +89,67 @@ class HomeFragment : Fragment() {
             val description = input_description.text.toString()
             val amount = input_amount.text.toString()
             var date = dateText.text.toString()
-            val type = dropdown_type.selectedItem.toString()
+
 
             if(description == "" || amount == ""){
                 Toast.makeText(context, "Please enter the fields", Toast.LENGTH_SHORT).show()
+            }else if(type == ""){
+                Toast.makeText(context, "Please select the type", Toast.LENGTH_SHORT).show()
             }else{
 
-                // Store to database
-
                 if(date == "Today"){
-                    var sdf = SimpleDateFormat("MMMM dd, yyyy")
+                    val sdf = SimpleDateFormat("MMMM dd, yyyy")
                     date = sdf.format(Date())
                 }
+
+                val db = DatabaseHelper(activity?.applicationContext!!, null)
+
+                db.insertData(money(description, amount.toFloat(), date, type))
+
+                onLoad()
 
                 Toast.makeText(context, "Saved!!", Toast.LENGTH_SHORT).show()
             }
 
+        }
+
+        button_location.setOnClickListener {
+        }
+
+    }
+
+    private fun onLoad() {
+        val db = DatabaseHelper(activity?.applicationContext!!, null)
+        var income = 0.0
+        var expense = 0.0
+
+        val data = db.readData()
+
+        for(i in 0 until data.size){
+            Log.i("message", data[i].type)
+            if(data[i].type == "Income"){
+                income += data[i].amount
+            }else{
+                expense += data[i].amount
+            }
+        }
+
+        AmountIncome.text = "$ " + String.format("%.2f", (income)).toDouble().toString()
+        AmountExpense.text = "$ " + String.format("%.2f", expense).toDouble().toString()
+        AmountBalance.text = "$ " + String.format("%.2f", (income-expense)).toDouble().toString()
+
+        val balance = AmountBalance.text.toString().split(" ").toTypedArray()
+
+        when {
+             balance[1].toFloat() < 0.0 -> {
+                AmountBalance.setTextColor(Color.parseColor("#ff0000"))
+            }
+            balance[1].toFloat() > 0.0 -> {
+                AmountBalance.setTextColor(Color.parseColor("#46A049"))
+            }
+            else -> {
+                AmountBalance.setTextColor(Color.parseColor("#000000"))
+            }
         }
     }
 }
